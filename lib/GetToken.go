0eafb5d6c10b271ff500d614656f457d2958985c
Shiwei Yu <shiwei1991@gmail.com>
2022-11-20 18:51:34 +0100

    home-tuya v0.0.9

--- lib/GetToken.go
+++ lib/GetToken.go
@@ -63,8 +63,6 @@ func InitToken() {
 }
 
 func TokenRefresh() {
-	// curl --request GET "https://openapi.tuyaeu.com/v1.0/token/7de3ad1ccee03bc32bb61645c19db038" --header "sign_method: HMAC-SHA256" --header "client_id: wqmd9rh4a9ebs0e1lt7i" --header "t: 1668965146662" --header "mode: cors" --header "Content-Type: application/json" --header "sign: EFEC8E34151A3B809D52A718C025B835FE530003BE2C05B8E843E67BAFC832F7" --header "access_token: "
-
 	method := "GET"
 	body := []byte(``)
 	req, _ := http.NewRequest(method, fmt.Sprintf("%v/v1.0/token/%v", os.Getenv("HOST_URL"), RefreshToken), bytes.NewReader(body))

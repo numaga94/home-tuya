8535c30cd113d146e60bebc262ab61a3eee9e3d1
Shiwei Yu <shiwei1991@gmail.com>
2022-11-21 18:13:57 +0100

    home-tuya v0.2.6

--- lib/InOpenHours.go
+++ lib/InOpenHours.go
@@ -8,10 +8,10 @@ import (
 func InOpenHours(beginHour int, endHour int) bool {
 	currentHour := time.Now().Hour()
 	if currentHour >= beginHour && currentHour <= endHour {
-		fmt.Printf("Current time is in open hours between %v and %v.", beginHour, endHour)
+		fmt.Println("Current time is in open hours between", beginHour, "and", endHour)
 		return true
 	} else {
-		fmt.Printf("Current time is out of the open hours between %v and %v.", beginHour, endHour)
+		fmt.Println("Current time is out of the open hours between", beginHour, "and", endHour)
 		return false
 	}
 }

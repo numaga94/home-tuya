14fa0ecdd7cf97fe5a226445baa6cf392c030952
Shiwei Yu <shiwei1991@gmail.com>
2022-12-16 16:09:36 +0100

    home-tuya v0.7.3

--- main.go
+++ main.go
@@ -71,78 +71,88 @@ func main() {
 		}
 	}()
 
-	http.HandleFunc("/ideal-temperature-humidity", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/ideal-temperature-humidity" {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		idealTemperatureHumidity := r.Header.Get("ideal-temperature-humidity")
+		openHours := r.Header.Get("open-hours")
+
+		if idealTemperatureHumidity != "" {
+			// if r.URL.Path != "/ideal-temperature-humidity" {
+			// 	responseText := fmt.Sprintln("404, not found.")
+			// 	log.Println(responseText)
+			// 	http.Error(w, responseText, http.StatusNotFound)
+			// }
+
+			switch r.Method {
+			case "GET":
+				currentTemperature := lib.GetCurrentTemperature()
+				currentHumidity := lib.GetCurrentHumidity()
+				responseText := fmt.Sprintf("ideal: %.1f degrees + %.1f %%H and current: %.1f degrees + %.1f %%H", idealTemperature, idealHumidity, currentTemperature, currentHumidity)
+				log.Println(responseText)
+				fmt.Fprintln(w, responseText)
+			case "POST":
+				// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+				if err := r.ParseForm(); err != nil {
+					fmt.Fprintf(w, "ParseForm() err: %v", err)
+					return
+				}
+				// parse values and reassign them to global variants
+				temp := r.FormValue("temperature")
+				humidity := r.FormValue("humidity")
+				if idealT, err := strconv.ParseFloat(temp, 64); err == nil {
+					idealTemperature = idealT
+				}
+				if idealH, err := strconv.ParseFloat(humidity, 64); err == nil {
+					idealHumidity = idealH
+				}
+				responseText := fmt.Sprintf("change ideal temperature to %.1f degrees and ideal humidity to %.1f %%H", idealTemperature, idealHumidity)
+				log.Println(responseText)
+				fmt.Fprintln(w, responseText)
+			default:
+				responseText := fmt.Sprintln("sorry, only GET and POST methods are supported.")
+				log.Println(responseText)
+				fmt.Fprintln(w, responseText)
+			}
+		} else if openHours != "" {
+			switch r.Method {
+			case "GET":
+				responseText := fmt.Sprintf("current open hours is between %d and %d.", openHoursBegin, openHoursEnd)
+				log.Println(responseText)
+				fmt.Fprintln(w, responseText)
+			case "POST":
+				// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+				if err := r.ParseForm(); err != nil {
+					fmt.Fprintf(w, "ParseForm() err: %v", err)
+					return
+				}
+				// parse values and reassign them to global variants
+				begin := r.FormValue("begin")
+				end := r.FormValue("end")
+				openHoursBegin, _ = strconv.Atoi(begin)
+				openHoursEnd, _ = strconv.Atoi(end)
+				responseText := fmt.Sprintf("change opens hours into between %d and %d.", openHoursBegin, openHoursEnd)
+				log.Println(responseText)
+				fmt.Fprintln(w, responseText)
+			default:
+				responseText := fmt.Sprintln("sorry, only GET and POST methods are supported.")
+				log.Println(responseText)
+				fmt.Fprintln(w, responseText)
+			}
+		} else {
 			responseText := fmt.Sprintln("404, not found.")
 			log.Println(responseText)
 			http.Error(w, responseText, http.StatusNotFound)
 		}
-
-		switch r.Method {
-		case "GET":
-			currentTemperature := lib.GetCurrentTemperature()
-			currentHumidity := lib.GetCurrentHumidity()
-			responseText := fmt.Sprintf("ideal: %.1f degrees + %.1f %%H and current: %.1f degrees + %.1f %%H", idealTemperature, idealHumidity, currentTemperature, currentHumidity)
-			log.Println(responseText)
-			fmt.Fprintln(w, responseText)
-		case "POST":
-			// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
-			if err := r.ParseForm(); err != nil {
-				fmt.Fprintf(w, "ParseForm() err: %v", err)
-				return
-			}
-			// parse values and reassign them to global variants
-			temp := r.FormValue("temperature")
-			humidity := r.FormValue("humidity")
-			if idealT, err := strconv.ParseFloat(temp, 64); err == nil {
-				idealTemperature = idealT
-			}
-			if idealH, err := strconv.ParseFloat(humidity, 64); err == nil {
-				idealHumidity = idealH
-			}
-			responseText := fmt.Sprintf("change ideal temperature to %.1f degrees and ideal humidity to %.1f %%H", idealTemperature, idealHumidity)
-			log.Println(responseText)
-			fmt.Fprintln(w, responseText)
-		default:
-			responseText := fmt.Sprintln("sorry, only GET and POST methods are supported.")
-			log.Println(responseText)
-			fmt.Fprintln(w, responseText)
-		}
 	})
 
-	http.HandleFunc("/open-hours", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/open-hours" {
-			responseText := fmt.Sprintln("404, not found.")
-			log.Println(responseText)
-			http.Error(w, responseText, http.StatusNotFound)
-			return
-		}
+	// http.HandleFunc("/open-hours", func(w http.ResponseWriter, r *http.Request) {
+	// 	if r.URL.Path != "/open-hours" {
+	// 		responseText := fmt.Sprintln("404, not found.")
+	// 		log.Println(responseText)
+	// 		http.Error(w, responseText, http.StatusNotFound)
+	// 		return
+	// 	}
 
-		switch r.Method {
-		case "GET":
-			responseText := fmt.Sprintf("current open hours is between %d and %d.", openHoursBegin, openHoursEnd)
-			log.Println(responseText)
-			fmt.Fprintln(w, responseText)
-		case "POST":
-			// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
-			if err := r.ParseForm(); err != nil {
-				fmt.Fprintf(w, "ParseForm() err: %v", err)
-				return
-			}
-			// parse values and reassign them to global variants
-			begin := r.FormValue("begin")
-			end := r.FormValue("end")
-			openHoursBegin, _ = strconv.Atoi(begin)
-			openHoursEnd, _ = strconv.Atoi(end)
-			responseText := fmt.Sprintf("change opens hours into between %d and %d.", openHoursBegin, openHoursEnd)
-			log.Println(responseText)
-			fmt.Fprintln(w, responseText)
-		default:
-			responseText := fmt.Sprintln("sorry, only GET and POST methods are supported.")
-			log.Println(responseText)
-			fmt.Fprintln(w, responseText)
-		}
-	})
+	// })
 
 	fmt.Println("http server is listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
